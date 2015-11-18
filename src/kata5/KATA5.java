a20cc653a95b1a781c524a34a2b996085df0ee4a
usuario <usuario@10.22.143.9>
2015-11-18 10:00:00 +0000

    Mails inserted in MAILS table

--- src/kata5/KATA5.java
+++ src/kata5/KATA5.java
@@ -2,15 +2,12 @@
 
 package kata5;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.*;
+import java.sql.*;
 
 public class KATA5 {
 
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         Connection conecta = DriverManager.getConnection("jdbc:sqlite:KATA5.DB");
         
@@ -22,6 +19,20 @@ public class KATA5 {
             System.out.println("ID = " + resultado.getInt("ID"));
             System.out.println("NAME = " + resultado.getString("NAME"));
         }
+        
+        String nameFile="C:\\Users\\usuario\\Downloads\\emailsfilev1.txt";
+        
+        BufferedReader reader = new BufferedReader(new FileReader(new File(nameFile)));
+        String mail;
+        while((mail = reader.readLine()) != null){
+            if(!mail.contains("@")) continue;
+            query = "INSERT INTO MAILS (MAIL) VALUES('" + mail + "')";
+            state.executeUpdate(query);
+        }
+        conecta.commit();
+        resultado.close();
+        state.close();
+        conecta.close();
     }
     
 }
